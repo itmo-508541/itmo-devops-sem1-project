ce20aeb7cf6f6a9904969fcc9c4ca0f9f6ac6864
Andrey <cshmuch@mail.ru>
2025-11-21 20:10:46 +0300

    revert StrictHostKeyChecking=no back

--- scripts/vm-deploy.sh
+++ scripts/vm-deploy.sh
@@ -6,10 +6,8 @@ while ! nc -z -v -w30 $1 22; do
   sleep 1
 done
 
-ssh-keyscan runner@$1 >> ~/.ssh/known_hosts
-scp -P 22 -r ./build/deploy.yml runner@$1:/tmp/deploy.yml
-
-ssh runner@$1 "if ! command -v docker; then sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin ; fi ;
+scp -o StrictHostKeyChecking=no -P 22 -r ./build/deploy.yml runner@$1:/tmp/deploy.yml
+ssh -o StrictHostKeyChecking=no runner@$1 "if ! command -v docker; then sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin ; fi ;
   cd /srv ;
   docker compose stop || true ;
   sudo mv /tmp/deploy.yml docker-compose.yml ;
