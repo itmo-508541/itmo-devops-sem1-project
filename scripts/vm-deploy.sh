9f72e53c007d5084fb04def6f7b418be311a5d0c
Andrey <cshmuch@mail.ru>
2025-11-21 16:55:13 +0300

    pass $SERVER_NAME

--- scripts/vm-deploy.sh
+++ scripts/vm-deploy.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-$SERVER_ADDRESS=$1
-while ! nc -z -v -w30 $SERVER_ADDRESS 22; do
+while ! nc -z -v -w30 $1 22; do
   sleep 1
 done
 
-ssh runner@$SERVER_ADDRESS -o StrictHostKeyChecking=no "curl -fsSL https://get.docker.com -o get-docker.sh ;
+ssh runner@$1 -o StrictHostKeyChecking=no "curl -fsSL https://get.docker.com -o get-docker.sh ;
   sudo bash ./get-docker.sh ;
   sudo chown runner:runner /srv ;
   rm /srv/deploy.yml || true"
 
 mkdir --parent ./build
 docker compose -f docker-compose.deploy.yml config > ./build/deploy.yml
-scp -P 22 -r ./build/deploy.yml runner@$SERVER_ADDRESS:/srv/deploy.yml
+scp -P 22 -r ./build/deploy.yml runner@$1:/srv/deploy.yml
 
-ssh runner@$SERVER_ADDRESS -o StrictHostKeyChecking=no "cd /srv ;
+ssh runner@$1 -o StrictHostKeyChecking=no "cd /srv ;
   docker compose stop || true ;
   rm docker-compose.yml || true ;
   mv deploy.yml docker-compose.yml ;
